9385c285dceaacb9b2867ce5693ca6b5ae7fdc89
Henrique Mayer <hmayer@gmail.com>
2025-10-28 16:20:22 -0300

    Initialize main.go with basic setup for avatar uploads and cached file cleanup

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"go-avatar-normalizer/handlers"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func init() {
+	clearCachedFiles()
+}
+
+func clearCachedFiles() {
+	retention := time.Now().Add(-1 * time.Hour)
+	files, _ := filepath.Glob(filepath.Join("./resources/images", "*"))
+	for _, f := range files {
+		info, _ := os.Stat(f)
+		if info.ModTime().Before(retention) {
+			fmt.Println("Removing old cached file", info.Name())
+			os.Remove(f)
+		}
+	}
+}
+
+func clearLoop() {
+	for {
+		time.Sleep(10 * time.Minute)
+		clearCachedFiles()
+	}
+}
+
+func main() {
+	http.HandleFunc("/avatar", handlers.AvatarUploadHandler)
+	fmt.Println("Routes defined")
+	clearLoop()
+	fmt.Println(http.ListenAndServe(":8000", nil))
+}
